5d09240fb2587c2b8a680ffadd628f5c01201471
rblavec <rblavec@isagri.fr>
2017-04-03 11:00:53 +0200

    rectification enumeration flags

--- Job Overview/Job Overview/Metier.cs
+++ Job Overview/Job Overview/Metier.cs
@@ -14,17 +14,18 @@ namespace Job_Overview
     [Flags]
     public enum Activités
     {
-        DBE,
-        ARF,
-        ANF,
-        DES,
-        INF,
-        ART,
-        ANT,
-        DEV,
-        RPT,
-        TES,
-        GDP
+        Aucun,
+        DBE = 0,
+        ARF = 1,
+        ANF = 2,
+        DES = 4,
+        INF = 8,
+        ART = 16,
+        ANT = 32,
+        DEV = 64,
+        RPT = 128,
+        TES = 256,
+        GDP = 512,
 
     }
     #endregion
@@ -32,11 +33,11 @@ namespace Job_Overview
     {
         static void MetierActivite()
         {
-            var ANA = new Employé (Activités.DBE | Activités.ARF | Activités.ANF);
-            var CDP = new Employé(Activités.ARF | Activités.ANF | Activités.ART | Activités.TES | Activités.GDP );
-            var DEV = new Employé(Activités.ANF | Activités.ART | Activités.ANT | Activités.DEV | Activités.TES );
-            var DES = new Employé(Activités.ANF | Activités.DES | Activités.INF );
-            var TES = new Employé(Activités.RPT | Activités.GDP );
+            var ANA = new Employé(Activités.DBE | Activités.ARF | Activités.ANF);
+            var CDP = new Employé(Activités.ARF | Activités.ANF | Activités.ART | Activités.TES | Activités.GDP);
+            var DEV = new Employé(Activités.ANF | Activités.ART | Activités.ANT | Activités.DEV | Activités.TES);
+            var DES = new Employé(Activités.ANF | Activités.DES | Activités.INF);
+            var TES = new Employé(Activités.RPT | Activités.GDP);
 
         }
 
