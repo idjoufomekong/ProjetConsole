00e0f6773ffd19b4db689f2f3c6eb3b5cbc0acab
rblavec <rblavec@isagri.fr>
2017-04-03 10:48:22 +0200

    Ajout des différentes classes

---
+++ Job Overview/Job Overview/Metier.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Job_Overview
+{
+
+    #region Enumeration
+    /// <summary>
+    /// Création d'un Type d'énuméré Flag afin de réaliser une association d'énumération pour les métiers (Concaténation Bit à Bit).
+    /// </summary>
+    [Flags]
+    public enum Activités 
+    {
+        DRE,
+        ARF,
+        ANF,
+        DES,
+        INF,
+        ART,
+        ANT,
+        DEV,
+        RPT,
+        TES,
+        GDP
+
+    }
+    #endregion
+    class Metier
+    {
+
+
+    }
+}
