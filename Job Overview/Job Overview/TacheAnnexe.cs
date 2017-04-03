f453fbaffd791258cf970acc7cf52eb0aeabb8fa
idjoufomekong <idjoufomekong@isagri.fr>
2017-04-03 12:22:03 +0200

    Ajout classes version et TacheAnnexe

---
+++ Job Overview/Job Overview/TacheAnnexe.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Job_Overview
+{
+      public  class TacheAnnexe
+    {
+        /// <summary>
+        /// Cette classe gère les différentes tâches annexes qui seront rajoutées au fur et 
+        /// à mesure par les employés
+        /// </summary>
+        #region Champs privés
+        private string _nomTâche;
+        private int _duréeTâche;
+        private DateTime _dateDébut;
+        private DateTime _dateFin;
+               
+        #endregion
+
+        #region Propriétés
+        public string NomTâche{get {return _nomTâche;}}
+        public int DuréeTâche { get { return _duréeTâche; } }
+        public DateTime DateDébut { get { return _dateDébut; } }
+        public DateTime DateFin { get { return _dateFin; } }
+
+        #endregion
+        #region Constructeurs
+        public TacheAnnexe(string nom, int durée, DateTime début, DateTime fin)
+        {
+            _nomTâche = nom ;
+            _duréeTâche = durée;
+            _dateDébut = début;
+            _dateFin = fin;
+        }
+
+        #endregion
+        #region Méthodes privées
+
+        #endregion
+        #region Méthodes publiques
+        #endregion
+
+    }
+}
---
+++ Job Overview/Job Overview/Version.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Job_Overview
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Version
+    {/// <summary>
+    /// La classe Version est une classe POCO qui stocke juste les différentes versions du logiciel. 
+    /// Elle n'aura donc que des propriétés automatiques
+    /// </summary>
+        #region Champs privés
+          
+        #endregion
+
+        #region propriétés
+        public int Numéro { get; set; }
+        public int Millésime { get; set; }
+        public DateTime DateDébut { get; set; }
+        public DateTime DatePubli { get; set; }
+
+        #endregion
+
+        #region Constructeurs
+       
+        #endregion
+
+        #region Méthodes privées
+
+        #endregion
+        
+        #region Méthodes publiques
+
+        #endregion
+    }
+}
