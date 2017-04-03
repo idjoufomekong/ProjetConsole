413e04d869dac2f54bcf6485e617d6f754f2dd41
idjoufomekong <idjoufomekong@isagri.fr>
2017-04-03 13:37:43 +0200

    nouvelle classe

---
+++ Job Overview/Job Overview/DescriptionTache.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Job_Overview
+{
+    class DescriptionTache
+    {
+        #region MyRegion
+
+        private int _numTâche;
+        private DateTime _dateDébut;
+        private DateTime _dateFin;
+        private int _duréePrévue;
+        #endregion
+
+        #region Propriétés
+        public int NumTâche { get; set; }
+        public int DateDébu { get; set; }
+        public int DateFin { get; set; }
+        public int DuréePrévue { get; set; }
+
+        #endregion
+
+        #region Constructeurs
+
+        #endregion
+
+        #region Méthodes privées
+
+        #endregion
+        #region Méthodes publiques
+
+        #endregion
+    }
+}
---
+++ Job Overview/Job Overview/TacheProduction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Job_Overview
+{
+    /// <summary>
+    /// Cette classe gère les tâches liées aux activités de production
+    /// </summary>
+    class TacheProduction
+    {
+        #region Propriétés
+        public int NumTâche { get; }
+        public int NomTâche { get; }
+        #endregion
+    }
+}
