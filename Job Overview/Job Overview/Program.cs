5249b9b08a0c5302ad5c1ea951d91767ae5fd137
rblavec <rblavec@isagri.fr>
2017-04-03 14:18:50 +0200

    ajout

--- Job Overview/Job Overview/Program.cs
+++ Job Overview/Job Overview/Program.cs
@@ -10,12 +10,25 @@ namespace Job_Overview
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Veuillez saisir le nombre de taches annexes");
+            int nombre = 0; // declaration et initialisation de nombre
 
+            List<>
+            while (nombre != 0)
+            {
+
+
+            }
+           
+          
+            Console.ReadKey();
+          
+            
             Console.WriteLine("Veuillez saisir les activités annexes (Code et Libellé :\n");
             Console.WriteLine("Veuillez saisir le code de l'activité :\n");
             //var GL = new Employé("Geneviève", "LECLERCQ", Metier._ANA);
 
-
+            Console.Clear();
 
             Console.WriteLine("test ok");
             Console.ReadKey();
